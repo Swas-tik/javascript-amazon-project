584d67a3b2b471d7d810ed36b4f56a2b8e72a34b
Swas-tik <thawaliswastik@gmail.com>
2024-06-12 13:26:26 +0530

    Make the cart quatity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,7 +78,13 @@ products.forEach((product)=>{
         })
         }
 
-        console.log(cart)
-        
+        let cartQuantity = 0;
+
+        cart.forEach((item)=>{
+          cartQuantity += item.quantity;
+        })
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
       })
     })
