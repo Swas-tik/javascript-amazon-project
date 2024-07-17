7c7fcee43281bba9fb064ac68b29ab0fe220a2b1
Swas-tik <thawaliswastik@gmail.com>
2024-07-17 21:20:17 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js';
-import '../data/backend-practice.js'
+//import '../data/backend-practice.js'
 
-renderOrderSummery();
-renderPaymentSummary();
+loadProducts(() => {
+    renderOrderSummery();
+    renderPaymentSummary();
+});
